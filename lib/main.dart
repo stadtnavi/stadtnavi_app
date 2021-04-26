e3fdcbb016ef7f2b006ded00c38c3ec0243c696f
Samuel Rioja <samuelriotz@hotmail.com>
2021-04-26 11:37:29 -0400

    change main theme

--- lib/main.dart
+++ lib/main.dart
@@ -124,10 +124,22 @@ Future<void> main() async {
 
   // Colors
   final theme = ThemeData(
-    primaryColor: const Color(0xff263238),
-    primaryColorLight: const Color(0xffeceff1),
-    accentColor: const Color(0xffd81b60),
+    primaryColor: const Color(0xff9bbf28),
+    accentColor: const Color(0xffec5188),
     backgroundColor: Colors.white,
+    textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(color: Color(0xFF000000)),
+          bodyText2: const TextStyle(color: Color(0xFF9EC528)),
+          headline6: const TextStyle(color: Colors.white),
+          subtitle1: const TextStyle(color: Colors.white),
+        ),
+    primaryTextTheme: ThemeData.light().primaryTextTheme.copyWith(
+          headline6: const TextStyle(color: Colors.white),
+          subtitle1: const TextStyle(color: Colors.white),
+        ),
+    primaryIconTheme: ThemeData.light().primaryIconTheme.copyWith(
+          color: const Color(0xFFFDFDFE),
+        ),
   );
 
   // Run app
