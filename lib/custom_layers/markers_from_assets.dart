a1274f0c6b1633571dbd9b1bb7943e438e11b5bf
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-09 17:29:49 -0400

    add extra information from properties

--- lib/custom_layers/markers_from_assets.dart
+++ lib/custom_layers/markers_from_assets.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:latlong/latlong.dart';
 import 'package:flutter/services.dart';
@@ -25,7 +24,17 @@ Future<List<CustomMarker>> markersFromAssets(String path) async {
       coordinate[1] as double,
       coordinate[0] as double,
     );
-    markers.add(CustomMarker(position, icons[iconId]));
+    markers.add(CustomMarker(
+      id: properties["id"] as String,
+      position: position,
+      image: icons[iconId],
+      name: properties["name"] as String,
+      nameEn: properties["name_en"] as String,
+      nameDe: properties["name_de"] as String,
+      popupContent: properties["popupContent"] as String,
+      popupContentEn: properties["popupContent_en"] as String,
+      popupContentDe: properties["popupContent_de"] as String,
+    ));
   }
   return markers;
 }
