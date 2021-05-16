17434bc47fdfd6b6956092062a192da7fee87b8d
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-16 04:09:20 -0400

    swap order

--- lib/custom_layers/pbf_layer/static_pbf_layer.dart
+++ lib/custom_layers/pbf_layer/static_pbf_layer.dart
@@ -10,23 +10,24 @@ extension LayerIdsToString on PBFLayerIds {
   String enumToString() {
     final Map<PBFLayerIds, String> enumStrings = {
       PBFLayerIds.bus: "Bus stops",
-      PBFLayerIds.carpool: "Carpool stops",
       PBFLayerIds.rail: "Train stations",
+      PBFLayerIds.carpool: "Carpool stops",
     };
     return enumStrings[this];
   }
-
 }
-   PBFLayerIds pbfLayerIdsstringToEnum(String id) {
-    final Map<String, PBFLayerIds> enumStrings = {
-      "BUS": PBFLayerIds.bus,
-      "CARPOOL": PBFLayerIds.carpool,
-      "RAIL": PBFLayerIds.rail,
-    };
-    return enumStrings[id];
-  }
+
+PBFLayerIds pbfLayerIdsstringToEnum(String id) {
+  final Map<String, PBFLayerIds> enumStrings = {
+    "BUS": PBFLayerIds.bus,
+    "CARPOOL": PBFLayerIds.carpool,
+    "RAIL": PBFLayerIds.rail,
+  };
+  return enumStrings[id];
+}
+
 final Map<PBFLayerIds, PBFLayer> pbfLayers = {
   PBFLayerIds.bus: PBFLayer(PBFLayerIds.bus),
-  PBFLayerIds.carpool: PBFLayer(PBFLayerIds.carpool),
   PBFLayerIds.rail: PBFLayer(PBFLayerIds.rail),
+  PBFLayerIds.carpool: PBFLayer(PBFLayerIds.carpool),
 };
