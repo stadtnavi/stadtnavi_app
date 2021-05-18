f8ea92b56f33c0f4324bacc33d28f59149ab40cb
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-17 23:02:51 -0400

    bike parking

---
+++ lib/custom_layers/pbf_layer/bike_parks/bike_parks_enum.dart
@@ -0,0 +1,24 @@
+
+enum BikeParkLayerIds {
+  covered,
+  notCovered
+}
+
+
+// BikeParkLayerIds bikeParkLayerIdsstringToEnum(String id) {
+//   final Map<String, BikeParkLayerIds> enumStrings = {
+//     "Parkhaus": BikeParkLayerIds.parkingGarage,
+//     "Parkplatz": BikeParkLayerIds.parkingSpot,
+//     "Wohnmobilparkplatz": BikeParkLayerIds.rvParking,
+//     "Park-Ride": BikeParkLayerIds.parkRide,
+//     "Tiefgarage": BikeParkLayerIds.undergroundCarPark,
+//     "Barrierefreier-Parkplatz": BikeParkLayerIds.barrierFreeParkingSpace,
+//     "Park-Carpool": BikeParkLayerIds.parkCarpool,
+//   };
+//   if (enumStrings[id] == null) {
+//     throw Exception(
+//       "Should never happened, there is no PBFParkingLayerIds for $id",
+//     );
+//   }
+//   return enumStrings[id];
+// }
