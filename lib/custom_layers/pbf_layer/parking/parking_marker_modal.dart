3c3fea568eb83ca349efc347031c733fe6f4ceaa
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-20 04:43:44 -0400

    adding parking popups

---
+++ lib/custom_layers/pbf_layer/parking/parking_marker_modal.dart
@@ -0,0 +1,136 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:stadtnavi_app/custom_layers/pbf_layer/parking/parking_feature_model.dart';
+import 'package:stadtnavi_app/custom_layers/pbf_layer/parking/parking_icons.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:trufi_core/l10n/trufi_localization.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ParkingMarkerModal extends StatelessWidget {
+  final ParkingFeature parkingFeature;
+  const ParkingMarkerModal({Key key, @required this.parkingFeature})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final localization = TrufiLocalization.of(context);
+    final localeName = localization.localeName;
+    String spaces;
+    if (parkingFeature.total != null) {
+      if (parkingFeature.free != null) {
+        spaces =
+            "${parkingFeature.free} of ${parkingFeature.total} parking spaces available";
+      } else {
+        spaces = "${parkingFeature.total} parking spaces";
+      }
+      if (parkingFeature.state == "closed") {
+        spaces += " (closed)";
+      }
+    }
+    String disabledSpaces;
+    if (parkingFeature.totalDisabled != null &&
+        parkingFeature.freeDisabled != null) {
+      disabledSpaces =
+          "${parkingFeature.freeDisabled} of ${parkingFeature.totalDisabled} wheelchair-accessible parking spaces available";
+    }
+    final notes = jsonDecode(parkingFeature.notes ?? "{}");
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: SvgPicture.string(
+                  parkingMarkerIcons[parkingFeature.type] ?? "",
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  parkingFeature.name,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              if (spaces != null)
+                Text(
+                  spaces,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              if (disabledSpaces != null)
+                Text(
+                  disabledSpaces,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              if (parkingFeature.openingHours != null)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Divider(),
+                    const Text(
+                      "OPENING HOURS",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      parkingFeature.openingHours.replaceAll("; ", "\n"),
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              if (notes[localeName] != null)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Divider(),
+                    Text(
+                      "${notes[localeName]}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              if (parkingFeature.url != null)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        launch(parkingFeature.url);
+                      },
+                      child: const Text(
+                        "More info",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/custom_layers/widget/marker_modal.dart
+++ lib/custom_layers/widget/marker_modal.dart
@@ -41,3 +41,20 @@ Future<void> showMarkerModal({
     builder: (BuildContext context) => MarkerModal(builder: builder),
   );
 }
+
+Future<void> showBottomMarkerModal({
+  BuildContext context,
+  Widget Function(BuildContext) builder,
+}) async {
+  showModalBottomSheet(
+    context: context,
+    backgroundColor: Colors.transparent,
+    barrierColor: Colors.transparent,
+    builder: (buildContext) => SafeArea(
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        child: builder(buildContext),
+      ),
+    ),
+  );
+}
