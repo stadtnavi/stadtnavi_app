931978646fc7cf1508462ddf6dfa10bca1bb7aac
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-20 02:03:20 -0400

    adding marker modal

---
+++ lib/custom_layers/widget/marker_modal.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_panel/scrollable_panel.dart';
+
+class MarkerModal extends StatelessWidget {
+  final Widget Function(BuildContext) builder;
+  const MarkerModal({Key key, @required this.builder}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final panelController = PanelController();
+    final mediaQuery = MediaQuery.of(context);
+    return ScrollablePanel(
+      controller: panelController,
+      onClose: () => Navigator.pop(context),
+      builder: (scrollablePanelContext, controller) {
+        return SingleChildScrollView(
+          controller: controller,
+          child: SizedBox(
+            height: mediaQuery.size.height + 1,
+            child: SafeArea(
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: builder(scrollablePanelContext),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Future<void> showMarkerModal({
+  BuildContext context,
+  Widget Function(BuildContext) builder,
+}) async {
+  return showDialog(
+    barrierColor: Colors.transparent,
+    useSafeArea: false,
+    context: context,
+    builder: (BuildContext context) => MarkerModal(builder: builder),
+  );
+}
