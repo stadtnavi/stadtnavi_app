f8f6b8e4a86a95ab10a370794deaa8717b6dc2a2
Samuel Rioja <samuelriotz@hotmail.com>
2021-05-26 04:43:00 -0400

    added impressum custom menu option

---
+++ lib/custom_social_media/impressum.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:trufi_core/models/social_media/social_media_item.dart';
+
+class ImpressumMedia implements SocialMediaItem {
+  final String _url;
+
+  ImpressumMedia(this._url);
+  @override
+  WidgetBuilder get buildIcon => (context) => const Icon(Icons.receipt_rounded);
+
+  @override
+  String getTitle(BuildContext context) {
+    return "Impressum";
+  }
+
+  @override
+  String get url => _url;
+}
