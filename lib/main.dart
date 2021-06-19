5e78ed98c3cb5249579e960c72af03ed96a5864c
Samuel Rioja <samuelriotz@hotmail.com>
2021-06-19 14:36:10 -0400

    adding implementation of the new menu style

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:stadtnavi_app/configuration_service.dart';
-import 'package:stadtnavi_app/custom_between_fab/report_defects_button.dart';
 import 'package:stadtnavi_app/custom_layers/map_layers/map_leyers.dart';
 import 'package:stadtnavi_app/custom_layers/static_layer.dart';
+import 'package:stadtnavi_app/custom_menu.dart';
 import 'package:stadtnavi_app/custom_search_location/online_search_location.dart';
-import 'package:stadtnavi_app/custom_social_media/youtube_social_media.dart';
 import 'package:stadtnavi_app/theme.dart';
-
-import 'package:trufi_core/models/social_media/facebook_social_media.dart';
-import 'package:trufi_core/models/social_media/instagram_social_media.dart';
-import 'package:trufi_core/models/social_media/twitter_social_media.dart';
 import 'package:trufi_core/trufi_app.dart';
-
 import 'custom_layers/services/graphl_client/hive_init.dart';
-import 'custom_social_media/impressum.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,20 +25,7 @@ Future<void> main() async {
         MapLayer(MapLayerIds.bike),
       ],
       searchLocationManager: OnlineSearchLocation(),
-      socialMediaItem: [
-        FacebookSocialMedia("https://www.facebook.com/stadtnavi/"),
-        InstagramSocialMedia("https://www.instagram.com/stadtnavi/"),
-        TwitterSocialMedia("https://twitter.com/stadtnavi"),
-        YoutubeSocialMedia(
-          "https://www.youtube.com/channel/UCL_K2RPU0pxV5VYw0Aj_PUA",
-        ),
-        ImpressumMedia("https://www.herrenberg.de/impressum"),
-      ],
-      customBetweenFabBuilder: (contexts) {
-        return Column(
-          children: const [ReportDefectsButoon()],
-        );
-      },
+      menuItems: menuItems,
     ),
   );
 }
