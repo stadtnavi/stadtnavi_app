3f4eb118aabc729fd6ef856ae752487d58be257f
Samuel Rioja <samuelriotz@gmail.com>
2021-06-28 16:39:30 -0400

    Feat/query graphql park (#266)
    * [Feature] fetch park data
    
    * [Feature] using fetchPark
    
    * adding parking live update
    
    Co-authored-by: GustavoTCh <TGustavoCh@gmail.com>

---
+++ lib/custom_layers/services/graphql_operation/queries/park_queries.dart
@@ -0,0 +1,28 @@
+const String parking = r'''
+ query parking(
+   $parkId: String!
+ ){
+  vehicleParking(id: $parkId) {
+    vehicleParkingId
+    name
+    lon
+    lat
+    capacity {
+      carSpaces
+      bicycleSpaces
+      wheelchairAccessibleCarSpaces
+    }
+    availability {
+      carSpaces
+      bicycleSpaces
+      wheelchairAccessibleCarSpaces
+    }
+    imageUrl
+    tags
+    anyCarPlaces
+    detailsUrl
+    note
+    openingHours
+  }
+}
+''';
