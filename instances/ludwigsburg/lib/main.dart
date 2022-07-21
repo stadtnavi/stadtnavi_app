d7303f7e37839afa0d64617d9f2d4929190637f6
Gustavo T <tgustavoch@gmail.com>
2022-07-21 16:05:52 -0400

    [Feature] update initHiveForFlutter for stadtnavi

--- instances/ludwigsburg/lib/main.dart
+++ instances/ludwigsburg/lib/main.dart
@@ -3,31 +3,23 @@ import 'package:latlong2/latlong.dart';
 import 'package:ludwigsburg/components/share_itinerary_button.dart';
 import 'package:ludwigsburg/configuration_routes.dart';
 
-import 'package:stadtnavi_core/base/custom_layers/cubits/custom_layer/custom_layer_local_storage.dart';
 import 'package:stadtnavi_core/base/custom_layers/pbf_layer/weather/weather_layer.dart';
-import 'package:stadtnavi_core/base/pages/home/cubits/map_route_cubit/map_route_cubit.dart';
-import 'package:stadtnavi_core/base/pages/home/cubits/payload_data_plan/setting_fetch_cubit.dart';
 import 'package:stadtnavi_core/base/pages/home/setting_payload/setting_panel/setting_panel.dart';
-import 'package:stadtnavi_core/base/pages/home/transport_selector/map_modes_cubit/map_modes_cubit.dart';
 import 'package:stadtnavi_core/consts.dart';
 import 'package:stadtnavi_core/stadtnavi_core.dart';
+import 'package:stadtnavi_core/stadtnavi_hive_init.dart';
 
-import 'package:trufi_core/base/utils/graphql_client/hive_init.dart';
 import 'package:trufi_core/base/widgets/drawer/menu/social_media_item.dart';
 import 'package:trufi_core/base/blocs/theme/theme_cubit.dart';
+import 'package:trufi_core/base/utils/certificates_letsencrypt_android.dart';
 
 import 'branding_ludwigsburg.dart';
 import 'static_layer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initHiveForFlutter(boxes: [
-    ...listPathsHive,
-    MapRouteCubit.path,
-    MapModesCubit.path,
-    SettingFetchCubit.path,
-    CustomLayerLocalStorage.path,
-  ]);
+  await CertificatedLetsencryptAndroid.workAroundCertificated();
+  await initHiveForFlutter();
   // TODO we need to improve disable fetch method
   WeatherLayer.isdisable = true;
   // TODO we need to improve enableCarpool
