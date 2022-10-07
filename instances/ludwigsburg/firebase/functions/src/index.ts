83a07e4043f1d144317a2fba9906689298a1898d
Samuel Rioja <samuelriotz@hotmail.com>
2022-10-07 22:21:57 +0200

    adding firebase notifications

---
+++ instances/ludwigsburg/firebase/functions/src/index.ts
@@ -0,0 +1,25 @@
+import * as functions from "firebase-functions";
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const onStartNotifications = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    const notification: {
+        title: string
+        body: string
+    } = { 
+        title: `Herzlich willkommen in Ludwigsburg!`, 
+        body: `Durch zahlreiche Veranstaltungen am Wochenende rechnen wir mit einem erhöhten Verkehrsaufkommen.
+
+        Nutzen Sie nach Möglichkeit die öffentlichen Verkehrsmittel oder das Fahrrad. Am Sonntag zwischen 13:00 und 18:00 Uhr wird der Bus-Takt für Sie verdichtet.
+        
+        Falls Sie mit dem Auto anreisen, finden Sie in der App zahlreiche ausgewiesene Parkmöglichkeiten.
+        
+         
+        
+        Wir freuen uns auf Ihren Besuch!`
+     }
+     const data={notifications:[notification]}
+    response.send(data);
+});
