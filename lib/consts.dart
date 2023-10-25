ca1d632123cbd34874754a283b17117ca3ba92d7
Gustavo T <tgustavoch@gmail.com>
2023-10-25 17:27:53 -0400

    [Feat] Migrate to otp 2.4

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,4 +1,4 @@
-const baseDomain = "api.stadtnavi.de";
+const baseDomain = "api.dev.stadtnavi.eu";
 String openTripPlannerUrl =
     "https://$baseDomain/routing/v1/router/index/graphql";
 String faresURL = "https://$baseDomain/fares";
