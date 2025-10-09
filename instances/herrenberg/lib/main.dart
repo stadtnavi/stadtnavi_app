fb713d15e42f317ede6e1ac8e017d0aae44a448d
Gustavo T. <tgustavoch@gmail.com>
2025-10-09 01:45:59 -0400

    [Feat] Configure core for stadtnavi

--- instances/herrenberg/lib/main.dart
+++ instances/herrenberg/lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:developer';
-
-import 'package:de_stadtnavi_herrenberg_internal/firebase_options.dart';
-import 'package:firebase_app_installations/firebase_app_installations.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:latlong2/latlong.dart';
+
+import 'package:trufi_core/consts.dart';
 import 'package:trufi_core/localization/app_localization.dart';
 import 'package:trufi_core/localization/language_bloc.dart';
 import 'package:trufi_core/default_theme.dart';
 import 'package:trufi_core/hive_init.dart';
+import 'package:trufi_core/pages/home/service/routing_service/otp_stadtnavi/graphql_plan_data_source.dart';
+import 'package:trufi_core/pages/home/widgets/routing_map/routing_map_controller.dart';
+import 'package:trufi_core/repositories/location/services/stadtnavi_photon_location_search_service.dart';
 import 'package:trufi_core/screens/route_navigation/route_navigation.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -17,39 +17,17 @@ import 'package:intl/date_symbol_data_local.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   const String appLocale = 'en';
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  await FirebaseMessaging.instance.getAPNSToken();
-  FirebaseMessaging.instance
-      .getToken()
-      .then((value) {
-        log("getToken: $value");
-      })
-      .catchError((error) {
-        log("catchError");
-      });
-  FirebaseInstallations.instance
-      .getId()
-      .then((value) {
-        log("Firebase Installation ID: $value");
-      })
-      .catchError((error) {
-        log("catchError");
-      });
-  await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-
   Intl.defaultLocale = appLocale;
   await initializeDateFormatting(appLocale);
   await initHiveForFlutter();
+  ApiConfig().configure(
+    baseDomain: "api.dev.stadtnavi.eu",
+    otpPath: "/otp/gtfs/v1",
+    originMap: const LatLng(48.5950, 8.8672),
+    locationSearchService: StadtnaviPhotonLocationSearchService(
+      photonUrl: 'https://photon-eu.stadtnavi.eu/pelias/v1/',
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -74,7 +52,16 @@ class MyApp extends StatelessWidget {
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: ThemeMode.system,
-        home: const RouteNavigationScreen(),
+        home: RouteNavigationScreen(
+          routingMapComponent: (controller, planRepository) {
+            return RoutingMapComponent(
+              controller,
+              customPlanRepository: StadtnaviGraphQLPlanDataSource(
+                ApiConfig().openTripPlannerUrl,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
