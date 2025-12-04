d2257fd4c5d4074848e83cdaaa46c3b055438c0f
Gustavo T. <tgustavoch@gmail.com>
2025-12-04 13:01:43 -0400

    fix(android): update NDK to 28 and suppress minSdk error for Rive 0.13.x

--- instances/herrenberg/android/app/build.gradle.kts
+++ instances/herrenberg/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "de.stadtnavi.herrenberg.internal"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.0.12433566"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -36,7 +36,7 @@ android {
         applicationId = "de.stadtnavi.herrenberg.internal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
